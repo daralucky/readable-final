18e842704c09e40e5c0794d244b23a7dfa273ecc
Dara Heng <daraheng.ncdd@gmail.com>
2017-09-06 09:20:30 +0700

    refactor: Prepare project structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Readable 02</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
