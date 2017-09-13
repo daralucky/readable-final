9ff19148dc29407fbb0130c74c2f35f7b55984bf
Dara Heng <daraheng.ncdd@gmail.com>
2017-09-13 08:23:40 +0700

    bug: refreshing bug

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import NavigationBar from './NavigationBar'
+import queryString from 'query-string'
 
 class DetailPage extends Component {
     render() {
-        console.log(JSON.stringify(this.props.match.params))
-        console.log(JSON.stringify(this.props.location))
+
+        const postId = this.props.match.params.postId
+        const { posts } = this.props
+        //console.log(JSON.stringify(this.props.match.params))
+        //console.log(JSON.stringify(this.props.location))
+
+        // console.log(JSON.stringify(this.props))
+
+
+        const currentPost = posts[postId]
+
         return (
             <div>
-                <NavigationBar />
-                <div id='detail-block'>
+                 <NavigationBar />
 
+                <div id='detail-block'>
+                    {console.log('HELLO: ' + JSON.stringify(this.props.match.params.postId))}
+                    {console.log('postID: ' + JSON.stringify(postId))}
                     <div className="my-post-list-page-header">
-                        <span style={{ color: 'DodgerBlue', fontWeight: 'bold', fontSize: 'x-large' }}> Post Title</span>
+                        <span style={{ color: 'DodgerBlue', fontWeight: 'bold', fontSize: 'x-large' }}>{currentPost.title}</span>
                     </div>
 
                     <div>
-                        bla bla bla
+                        {currentPost.body}
                     </div>
 
                 </div>
@@ -25,4 +38,18 @@ class DetailPage extends Component {
     }
 }
 
-export default DetailPage
+
+const mapStateToProps = state => {
+    return {
+        posts: state.posts
+    }
+}
+
+
+export default connect(
+    mapStateToProps
+)(DetailPage)
+
+
+
+//export default DetailPage
